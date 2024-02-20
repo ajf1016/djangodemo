ba3739af43fcf8f28f8e7653264d386f83354ba0
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-02-21 02:21:12 +0530

    dynamicaly handling urls

--- demo1/demo1/urls.py
+++ demo1/demo1/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
-
-
-
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("web.urls"))
+    path('', include("web.urls", namespace="web"))
 ]
--- demo1/web/urls.py
+++ demo1/web/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from web.views import webIndex,htmlPage,aboutPage
 
 
+app_name = "web"
+
 urlpatterns = [
     path('', webIndex),
-    path('web/', htmlPage),
-    path('web/about', aboutPage)
+    path('web/', htmlPage,name="home"),
+    path('web/about-us', aboutPage,name="about")
 ]
