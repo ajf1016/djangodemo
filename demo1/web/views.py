f26612b40a7f78034c5abf03d6d02a3f06a34c95
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-02-24 23:51:59 +0530

    web / views updated

--- demo1/web/views.py
+++ demo1/web/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 
-def webIndex(request):
-    return HttpResponse("Hello World!, from Web app<a href='http://127.0.0.1:8000/web/'>Click for web page</a>")
-
 def htmlPage(request):
     return render(request, 'index.html')
 
